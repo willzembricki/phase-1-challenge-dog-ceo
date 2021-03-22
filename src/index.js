0f1a868bc51d8ab3f1f7ab54aee6fd17b862e0fb
William Zembricki <wzembricki@gmail.com>
2021-03-22 14:23:26 -0400

    challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+// challenge 1 
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+let dogList = document.querySelector("div#dog-image-container")
+
+fetch(imgUrl)
+    .then(res => res.json())
+    .then(function(dogResponse){
+       let dogArray = [] 
+       dogResponse.message.forEach(function(link){
+          dogArray.push(link)
+          
+       })
+       
+        dogArray.forEach(function(e ,i ){
+            let blankLi = document.createElement("li")
+            let blankImg = document.createElement("img")
+                blankImg.src = dogArray[i]
+                blankImg.alt = "dog"
+            
+                blankLi.append(blankImg)
+            
+                dogList.append(blankLi)
+        })
+    })
+
+// challenge 2 
